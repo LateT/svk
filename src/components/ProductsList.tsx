d151154f85b9443880750c0e54ecb116fd139030
Lauri Timonen <late.timonen@gmail.com>
2025-02-03 20:01:50 +0200

    == to ===, map to forEach, unnecessary string concats to ``

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Tuote } from '../types/Tuote';
 import { Button, ListGroup } from 'react-bootstrap';
 
@@ -7,9 +7,9 @@ const ProductsList = ({ products, startIndex, setFrom }: { products: Tuote[], st
     
     const ItemList: React.ReactElement[] = [];
 
-    products.map((p, index) => {
+    products.forEach((p, index) => {
         console.log(p);
-        if (p.ktuote && p.ean == p.ktuote?.ean) {
+        if (p.ktuote && p.ean === p.ktuote?.ean) {
             let priceToCompare = 0;
             if (p.ktuote.pricing.discount) {
                 priceToCompare = p.ktuote.pricing.discount.price;
@@ -20,15 +20,15 @@ const ProductsList = ({ products, startIndex, setFrom }: { products: Tuote[], st
                 ItemList.push(<ListGroup.Item key={index}>
                     {p.name} | S: <span style={{ color: "green" }}>{p.price}€</span> | K: <span style={{ color: "red" }}>{priceToCompare}€</span>
                 </ListGroup.Item>)
-            } else if (priceToCompare == p.price) {
-                ItemList.push(<ListGroup.Item key={index}>{p.name + " | S:" + p.price + "€ " + " | K: " + priceToCompare + "€"}</ListGroup.Item>)
+            } else if (priceToCompare === p.price) {
+                ItemList.push(<ListGroup.Item key={index}>{`${p.name} | S: ${p.price}€ | K: ${priceToCompare}€`}</ListGroup.Item>)
             } else if (priceToCompare < p.price) {
                 ItemList.push(<ListGroup.Item key={index}>
                     {p.name} | S: <span style={{ color: "red" }}>{p.price}€</span> | K: <span style={{ color: "green" }}>{priceToCompare}€</span>
                 </ListGroup.Item>)
             }
         } else {
-            ItemList.push(<ListGroup.Item key={index}>{p.name + " | S:" + p.price + "€ " + " | K: ?€"}</ListGroup.Item>)
+            ItemList.push(<ListGroup.Item key={index}>{`${p.name} | S: ${p.price}€ | K: ?€`}</ListGroup.Item>)
         }
     });
 
@@ -37,7 +37,7 @@ const ProductsList = ({ products, startIndex, setFrom }: { products: Tuote[], st
             <ListGroup>
                 {ItemList}
             </ListGroup>
-            <Button disabled={startIndex == 0} onClick={() => setFrom(startIndex - 10)}>{"<"}</Button> <Button onClick={() => setFrom(startIndex + 10)} disabled={products.length < 10}>{">"}</Button>
+            <Button disabled={startIndex === 0} onClick={() => setFrom(startIndex - 10)}>{"<"}</Button> <Button onClick={() => setFrom(startIndex + 10)} disabled={products.length < 10}>{">"}</Button>
         </>
     )
 }
