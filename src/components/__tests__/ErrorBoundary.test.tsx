a92f5cfdec37c4330dd4ef37913c8bc0086afbc0
Lauri Timonen <late.timonen@gmail.com>
2025-02-04 19:09:50 +0200

    add describe to the test

--- src/components/__tests__/ErrorBoundary.test.tsx
+++ src/components/__tests__/ErrorBoundary.test.tsx
@@ -3,16 +3,19 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ErrorBoundary from '../ErrorBoundary';
 
-const ProblemChild = () => {
-  throw new Error('Error thrown from problem child');
-};
+describe("Error boundary tests", () => {
 
-test('Renders fallback UI when there is an error', () => {
-  const { getByText } = render(
-    <ErrorBoundary>
-      <ProblemChild />
-    </ErrorBoundary>
-  );
-
-  expect(getByText('Something went wrong.')).toBeInTheDocument();
-});
+  const ProblemChild = () => {
+    throw new Error('Error thrown from problem child');
+  };
+  
+  test('Renders fallback UI when there is an error', () => {
+    const { getByText } = render(
+      <ErrorBoundary>
+        <ProblemChild />
+      </ErrorBoundary>
+    );
+  
+    expect(getByText('Something went wrong.')).toBeInTheDocument();
+  });
+})
